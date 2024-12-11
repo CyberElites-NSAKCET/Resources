0e7288932c7472c74e53bc1e516650bd24d19cc9
Mohammed Abdul Raqeeb <raqeeb2709@gmail.com>
2024-12-11 21:32:50 +0530

    Corrected the files and directory names; Added simple html_body_file checker

---
+++ extract_certify_and_email.py
@@ -0,0 +1,82 @@
+import csv
+import os
+from Certificate_Generator.certificate_generator import get_files, get_single_file
+from Email_Sender.send_email import check_csv, sort_csv
+
+
+def extract_spreadsheet(spreadsheet_file_path, tosend_csv_path, wordlist_file_path):
+    # Process the file
+    with open(spreadsheet_file_path, mode='r', encoding="utf-8") as sheet_csv_file:
+        reader = csv.DictReader(sheet_csv_file)
+        try:
+            # Create and write to 'tosend.csv'
+            with open(tosend_csv_path, mode='w', newline='') as tosend_csv_file:
+                csv_writer = csv.writer(tosend_csv_file)
+                csv_writer.writerow(['Full Name', 'Email'])  # Write header row
+                
+                # Create and write to 'wordlist.txt'
+                with open(wordlist_file_path, mode='w') as wordlist_file:
+                    for row in reader:
+                        if row['Attendance'].strip().upper() == 'TRUE':  # Check Attendance
+                            full_name = row['Full Name'].strip().title()
+                            csv_writer.writerow([full_name, row['Email']])
+                            wordlist_file.write(f"{full_name}\n")
+                    print("\n\'Full Name\' column successfully written to wordlist file.")
+                    
+                print("\'Full Name\' and \'Email\' columns successfully extracted to \'tosend.csv\' file.")
+            print("\nSuccessfully extracted the spreadsheet file.\n")
+        except:
+            print("\nFailed to write to \"tosend.csv\" file.\nEnsure that the file is not open on the system.\n")
+            exit(1)
+
+
+# === MAIN ENTRY POINT ===
+if __name__ == "__main__":
+    
+    if not os.getcwd()[-9:] == "Resources":
+        print("\nPlease change your working directory to the main repository.\n\nExiting...\n")
+        exit(1)
+        
+    EMAIL_SUBJECT = "Subject"
+
+    CERTIFICATE_GENERATOR_DIRECTORY_PATH = os.path.join(os.getcwd(), "Certificate_Generator")
+    EMAIL_SENDER_DIRECTORY_PATH = os.path.join(os.getcwd(), "Email_Sender")
+    CERTIFICATE_EMAIL_AUTOMATION_DIR_PATH = os.path.join(os.getcwd(),"Certificate_Email_Automation")
+    SPREADSHEET_DIRECTORY_PATH = os.path.join(CERTIFICATE_EMAIL_AUTOMATION_DIR_PATH, "Spreadsheet")
+    WORDLIST_DIRECTORY_PATH = os.path.join(CERTIFICATE_EMAIL_AUTOMATION_DIR_PATH, "Wordlist")
+    
+    print("\n" + " Certificate_Email_Automation ".center(40, "-"))
+    
+    os.makedirs(SPREADSHEET_DIRECTORY_PATH, exist_ok=True)
+    os.makedirs(WORDLIST_DIRECTORY_PATH, exist_ok=True)
+    
+    csv_files = get_files(SPREADSHEET_DIRECTORY_PATH, 'CSV')
+    spreadsheet_file = get_single_file('Spreadsheet', SPREADSHEET_DIRECTORY_PATH, 'CSV')
+    spreadsheet_file_path = os.path.join(SPREADSHEET_DIRECTORY_PATH, spreadsheet_file)
+    
+    text_files = get_files(WORDLIST_DIRECTORY_PATH, 'TXT')
+    if not text_files:
+        with open(os.path.join(WORDLIST_DIRECTORY_PATH, 'wordlist.txt'), 'w') as tosend_csv_file:
+            pass
+        
+    wordlist_file_path = os.path.join(WORDLIST_DIRECTORY_PATH, 'wordlist.txt')
+    tosend_csv_path = os.path.join(CERTIFICATE_EMAIL_AUTOMATION_DIR_PATH, "tosend.csv")
+    certificate_script_path = os.path.join(CERTIFICATE_GENERATOR_DIRECTORY_PATH, "certificate_generator.py")
+    email_script_path = os.path.join(EMAIL_SENDER_DIRECTORY_PATH, "send_email.py")
+    
+    # Open the file and ensure it has the correct contents as needed
+    check_csv(spreadsheet_file_path, "Other", "Attendance")
+    
+    extract_spreadsheet(spreadsheet_file_path, tosend_csv_path, wordlist_file_path)
+
+    sort_csv(tosend_csv_path)
+    
+    try:
+        certificate_script_status = os.system(f"python {certificate_script_path} extract_certify_and_email_script")
+        if certificate_script_status == 0:
+            os.system(f"python {email_script_path} extract_certify_and_email_script \"{EMAIL_SUBJECT}\"")
+    except KeyboardInterrupt:
+        exit(1)
+    except Exception as e:
+        print(f"\n\nAn error occured while executing the script.\n{e}\n")
+        exit(1)
